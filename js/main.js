6dae6f02d65004a436f5f4bcdedf4973de7e3d89
callidora-idsardi <114117190+callidora-idsardi@users.noreply.github.com>
2023-12-08 02:29:36 -0500

    finished hello 3D animation, sidebar and hero area

--- js/main.js
+++ js/main.js
@@ -1,98 +1,25 @@
+
+(() => {
+    console.log("IIFE Fired");
+
+
+
+// Hamburger Menu Start
 console.log('js is wired!');
 
-let projectLink = document.querySelectorAll('.project-link'),
-    infoDiv = document.querySelector('#project-con');
-
-// images are filler for now 
-
-let projects = {
-    squeezit: {
-        Type: 'HTML, CSS, JS, UX/UI, Brand Identity',
-        Image: 'img/project1.jpg',
-        Name: 'Squeezit',
-        Info: 'Developed a website & whole new brand refresh for SQUEEZIT, a squeezable juice company made by General Mills and originally marketed in 1985.',
-    },
-
-    relaxify: {
-        Type: 'HTML, CSS, JS',
-        Image: 'img/project1.jpg',
-        Name: 'Relaxify',
-        Info: 'Developed a website & whole new brand refresh for SQUEEZIT, a squeezable juice company made by General Mills and originally marketed in 1985. ',
-    },
-
-    clearbuds: {
-        Type: 'HTML, CSS, JS, UX/UI, Brand Identity',
-        Image: 'img/project1.jpg',
-        Name: 'Crystal Clear Buds',
-        Info: 'Developed a website & whole new brand refresh for SQUEEZIT, a squeezable juice company made by General Mills and originally marketed in 1985. ',
-    },
-
-    wormcafe: {
-        Type: 'HTML, CSS, UX/UI, Brand Identity',
-        Image: 'img/project1.jpg',
-        Name: 'Book Worm Cafe',
-        Info: 'Developed a website & whole new brand refresh for SQUEEZIT, a squeezable juice company made by General Mills and originally marketed in 1985. ',
-    },
-};
-
-// functions go here
-function listInfo() {
-    // empty out the list
-    infoDiv.innerHTML = "";
-
-    // name of project
-    let header = document.createElement('h2');
-    header.textContent = projects[this.dataset.team]['Name'];
-    infoDiv.appendChild(header);
-
-    // portfolio projects
-    let image = document.createElement('img');
-    image.src = projects[this.dataset.team]['Image'];
-    infoDiv.appendChild(image);
-
-    // project information
-    let info = document.createElement('p');
-    bio.textContent = projects[this.dataset.team]['Info'];
-    infoDiv.appendChild(info);
-
-    // link to github repos
-    let link = document.createElement('a');
-    link.href = projects[this.dataset.team]['Link'];
-    link.textContent = 'Github';
-    infoDiv.appendChild(link);
-
-}
-
-
-
-// add event handling here
-projectLink.forEach(projectButton => projectButton.addEventListener('click', listInfo)),
-
-
-
-    // HAMBURGER MENU
-
     (function () {
-        "use strict";
-        console.log("fired");
+    let burgerButton = document.getElementById('burger-button');
+    let burgerCon = document.getElementById('burger-con');
+    let burgerBg = document.getElementById('burger-bg');
 
-        let button = document.querySelector("#button");
-        let burgerCon = document.querySelector("#burger-con");
+    function hamburgerMenu() {
+        burgerButton.classList.toggle('transform'); //transforms button on click 
+        burgerCon.classList.toggle('transform');    //transforms button information on click 
+        burgerBg.classList.toggle('transform-bg'); //transforms button container background on click 
+    }
 
-        function hamburgerMenu() {
-            burgerCon.classList.toggle("slide-toggle");
-            button.classList.toggle("expanded");
-        };
+    burgerButton.addEventListener('click', hamburgerMenu);
+})();
 
-        // let hamburgerMenu = () => {
-        // 	burgerCon.classList.toggle("slide-toggle");
-        // 	button.classList.toggle("expanded");
-        // };
 
-        button.addEventListener("click", hamburgerMenu, false);
-    })();
-
-//Can also be written like this:
-//(() => {  })();
-
-// Info about IIFE https://flaviocopes.com/javascript-iife/
+})();
